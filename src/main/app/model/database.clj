b78f44d5607d5460941341272425a01d2cc810df
Niluka Satharasinghe (folcon) <folcon@gmail.com>
2020-04-10 21:39:51 +0100

    Added handling for heroku's DATABASE_URL

--- src/main/app/model/database.clj
+++ src/main/app/model/database.clj
@@ -7,7 +7,43 @@
             [next.jdbc.prepare :as p]
             [clojure.string :as str]
             [honeysql.core :as sql]
-            [honeysql-postgres.format]))
+            [honeysql-postgres.format])
+  (:import [java.net URI]))
+
+
+(defn create-uri [url] (URI. url))
+
+(defn parse-username-and-password [db-uri]
+  (str/split (.getUserInfo db-uri) #":"))
+
+(defn parse-db-name [db-uri]
+  (str/replace (.getPath db-uri) "/" ""))
+
+(defn parse-host [db-uri]
+  (.getHost db-uri))
+
+(defn parse-port [db-uri]
+  (.getPort db-uri))
+
+(defn hikari-connection-map
+  "Converts Heroku's DATABASE_URL to a map that you can pass to hikari"
+  [heroku-database-url]
+  (let [db-uri (create-uri heroku-database-url)
+        host    (parse-host db-uri)
+        port    (parse-port db-uri)
+        db-name (parse-db-name db-uri)
+        [username password] (parse-username-and-password db-uri)]
+    {:username      username
+     :password      password
+     :server-name   host
+     :port-number   port
+     :database-name db-name
+     :sslmode       "require"}))
+
+(defn process-database-spec [{:keys [jdbc-url] :as db-spec}]
+  (if jdbc-url
+    (hikari-connection-map jdbc-url)
+    db-spec))
 
 (defn datasource-options []
   (merge {:auto-commit        true
@@ -21,7 +57,7 @@
           :pool-name          "db-pool"
           :adapter            "postgresql"
           :register-mbeans    false}
-    (:database-spec config)))
+    (process-database-spec (:database-spec config))))
 
 (defstate pool
   :start (pool/make-datasource (datasource-options))
