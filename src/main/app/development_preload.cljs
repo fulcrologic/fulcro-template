7345dc5174f87e70e79997a81e1b0be64645ef49
Tony Kay <tony.kay@gmail.com>
2025-09-04 11:07:13 +0300

    updated to latest deps

--- src/main/app/development_preload.cljs
+++ src/main/app/development_preload.cljs
@@ -4,6 +4,5 @@
     [taoensso.timbre :as log]))
 
 (js/console.log "Turning logging to :debug (in app.development-preload)")
-(log/set-level! :debug)
 (log/merge-config! {:output-fn ts/prefix-output-fn
                     :appenders {:console (ts/console-appender)}})
